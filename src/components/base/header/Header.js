df4d747b7dcb20cc0e12369b4e7e67047bcbe77b
Greg <greg@Gregs-MacBook-Pro.local>
2018-11-13 07:19:35 -0500

    Built layout

---
+++ src/components/base/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        App Header
+      </div>
+    );
+  }
+}
+
+export default Header;
